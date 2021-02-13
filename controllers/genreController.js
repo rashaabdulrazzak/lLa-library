29276d57f0c45bc9d8abeb58700332ffdf22975f
rashaabdulrazzak <rasha.abdulrazzak.com>
2021-02-13 16:24:44 +0300

    genre detail page controller and view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,7 @@
 var Genre = require("../models/genre");
+var Book = require("../models/book"); // we call books here as in genre detail page we need to list all the book with that genre
+
+var async = require("async");
 
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
@@ -18,7 +21,34 @@ exports.genre_list = function (req, res, next) {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books: function (callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        var err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Display Genre create form on GET.
